b1c2e09c2bae129c2a0623c349b17e65cbb77d09
James Widener <jameskwidener@gmail.com>
2017-12-20 10:32:55 -0500

    .travis.yml and ReverseString changed

--- src/main/java/quiz/ReverseString.java
+++ src/main/java/quiz/ReverseString.java
@@ -1,7 +1,22 @@
 package quiz;
 
+import java.util.ArrayList;
+
 public class ReverseString {
     public String reverse(String input) {
-        return new StringBuffer(input).reverse().toString();
+        //return new StringBuffer(input).reverse().toString();
+        if (input.equals(""))
+            return "";
+        else {
+            ArrayList<Character> chars = new ArrayList<>();
+            for (int i = 0; i < input.length(); i++) {
+                chars.add(0, input.charAt(i));
+            }
+            StringBuilder builder = new StringBuilder(chars.size());
+            for (Character c : chars) {
+                builder.append(c);
+            }
+            return builder.toString();
+        }
     }
 }
